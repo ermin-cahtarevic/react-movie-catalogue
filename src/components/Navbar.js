5f887a2a53c83ae125783ad8ae49ac98f05d59e7
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-30 12:40:54 +0200

    Fix home page bug after searching for movie

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/navbar.css';
 import Search from './Search';
+import fetchMovies from '../actions/fetchMovies';
+import { useDispatch } from 'react-redux';
 
 const Navbar = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    fetchMovies()(dispatch);
+  };
+
   return (
     <div className="navbar">
-      <Link to="/" className="logo">Movie Catalogue</Link>
+      <Link
+        to="/"
+        className="logo"
+        onClick={() => handleClick()}
+      >
+      Movie Catalogue
+      </Link>
       {
         location.pathname === '/' ? (
           <Search />
